c1925f5fd4cac66eb5c342cf10aa49638da38299
Mks <maxnxi@icloud.com>
2020-11-10 13:40:53 +0300

    Initial project setup and BeginRunVC UI done

---
+++ Treads/BeginRunVC.swift
@@ -0,0 +1,20 @@
+//
+//  BeginRunVC.swift
+//  Treads
+//
+//  Created by Maksim Ponomarev on 10.11.2020.
+//
+
+import UIKit
+
+class BeginRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
