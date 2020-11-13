345d3f2260236c9c3f107257e7ba3eec4d4efe10
Mks <maxnxi@icloud.com>
2020-11-14 02:03:21 +0300

    tableview for Log done

--- Treads/Controller/LogTableVC.swift
+++ Treads/Controller/LogTableVC.swift
@@ -6,15 +6,43 @@
 //
 
 import UIKit
+import Foundation
 
-class LogTableVC: UIViewController {
+class LogTableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    @IBOutlet weak var logTbaleView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        logTbaleView.delegate = self
+        logTbaleView.dataSource = self
+        
+        
 
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Run.getAllRuns()?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = logTbaleView.dequeueReusableCell(withIdentifier: "logCell") as? LogCell {
+            guard let run = Run.getAllRuns()?[indexPath.row] else {
+                return LogCell()
+            }
+            cell.configure(run: run)
+            return cell
+        } else {
+            debugPrint("Error fillng cell")
+            return LogCell()
+        }
+    }
   
 
 }
+
+
+
